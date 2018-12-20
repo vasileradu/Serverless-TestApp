0521d935bec8f92f65a291d9a410d73d32daf246
Radu Vasile <radu.vasile@qiagen.com>
2018-12-20 13:02:40 +0200

    Added Auth lib for data access and repository;

---
+++ TestApp.Core/TestApp.Core.Auth/Models/TestAppAuthContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TestApp.Core.Auth.Models
+{
+    public partial class TestAppAuthContext : DbContext
+    {
+        public TestAppAuthContext()
+        {
+        }
+
+        public TestAppAuthContext(DbContextOptions<TestAppAuthContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(256);
+
+                entity.Property(e => e.Username)
+                    .IsRequired()
+                    .HasMaxLength(256);
+            });
+        }
+    }
+}
---
+++ TestApp.Core/TestApp.Core.Auth/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestApp.Core.Auth.Models
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
