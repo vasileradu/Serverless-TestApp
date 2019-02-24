0d52890b269223e5b0cbad0a178b7d8eb303f1de
Radu Vasile <radu.vasile@qiagen.com>
2019-02-24 15:56:53 +0200

    Upgraded vm types;

--- Scripts/azure/CreateVms.ps1
+++ Scripts/azure/CreateVms.ps1
@@ -1,17 +1,17 @@
  # monolith
- az vm create --resource-group TestApp --name "monolith" --location "westeurope" --image win2016datacenter --size Standard_B4ms --admin-username "TestAppAdmin" --admin-password "Pa55word!Pa55word!" --storage-sku Standard_LRS --nsg "TestApp-ngs" --vnet-name "TestApp-vnet" --private-ip-address 10.0.0.4
+ az vm create --resource-group TestApp --name "monolith" --location "westeurope" --image win2016datacenter --size Standard_B8ms --admin-username "TestAppAdmin" --admin-password "Pa55word!Pa55word!" --storage-sku Standard_LRS --nsg "TestApp-ngs" --vnet-name "TestApp-vnet" --subnet "TestApp-subnet" --private-ip-address 10.0.0.4
  
  # service.analysis
- az vm create --resource-group TestApp --name "analysis" --location "westeurope" --image win2016datacenter --size Standard_B4ms --admin-username "TestAppAdmin" --admin-password "Pa55word!Pa55word!" --storage-sku Standard_LRS --nsg "TestApp-ngs" --vnet-name "TestApp-vnet" --private-ip-address 10.0.0.5
+ az vm create --resource-group TestApp --name "analysis" --location "westeurope" --image win2016datacenter --size Standard_A2m_v2 --admin-username "TestAppAdmin" --admin-password "Pa55word!Pa55word!" --storage-sku Standard_LRS --nsg "TestApp-ngs" --vnet-name "TestApp-vnet" --subnet "TestApp-subnet" --private-ip-address 10.0.0.5
  
  # service.auth
- az vm create --resource-group TestApp --name "auth" --location "westeurope" --image win2016datacenter --size Standard_B1s --admin-username "TestAppAdmin" --admin-password "Pa55word!Pa55word!" --storage-sku Standard_LRS --nsg "TestApp-ngs" --vnet-name "TestApp-vnet" --private-ip-address 10.0.0.6
+ az vm create --resource-group TestApp --name "auth" --location "westeurope" --image win2016datacenter --size Standard_B1s --admin-username "TestAppAdmin" --admin-password "Pa55word!Pa55word!" --storage-sku Standard_LRS --nsg "TestApp-ngs" --vnet-name "TestApp-vnet" --subnet "TestApp-subnet" --private-ip-address 10.0.0.6
   
  # service.reports
- az vm create --resource-group TestApp --name "reports" --location "westeurope" --image win2016datacenter --size Standard_B2s --admin-username "TestAppAdmin" --admin-password "Pa55word!Pa55word!" --storage-sku Standard_LRS --nsg "TestApp-ngs" --vnet-name "TestApp-vnet" --private-ip-address 10.0.0.7
+ az vm create --resource-group TestApp --name "reports" --location "westeurope" --image win2016datacenter --size Standard_B2s --admin-username "TestAppAdmin" --admin-password "Pa55word!Pa55word!" --storage-sku Standard_LRS --nsg "TestApp-ngs" --vnet-name "TestApp-vnet" --subnet "TestApp-subnet" --private-ip-address 10.0.0.7
  
  # service.files
- az vm create --resource-group TestApp --name "files" --location "westeurope" --image win2016datacenter --size Standard_B2s --admin-username "TestAppAdmin" --admin-password "Pa55word!Pa55word!" --storage-sku Standard_LRS --nsg "TestApp-ngs" --vnet-name "TestApp-vnet" --private-ip-address 10.0.0.8 
+ az vm create --resource-group TestApp --name "files" --location "westeurope" --image win2016datacenter --size Standard_B2s --admin-username "TestAppAdmin" --admin-password "Pa55word!Pa55word!" --storage-sku Standard_LRS --nsg "TestApp-ngs" --vnet-name "TestApp-vnet" --subnet "TestApp-subnet" --private-ip-address 10.0.0.8 
  
  # jmeter
- az vm create --resource-group TestApp --name "jmeter" --location "westeurope" --image win2016datacenter --size Standard_B2s --admin-username "TestAppAdmin" --admin-password "Pa55word!Pa55word!" --storage-sku Standard_LRS --nsg "TestApp-ngs" --vnet-name "TestApp-vnet" --private-ip-address 10.0.0.9
+ az vm create --resource-group TestApp --name "jmeter" --location "westeurope" --image win2016datacenter --size Standard_D1_v2 --admin-username "TestAppAdmin" --admin-password "Pa55word!Pa55word!" --storage-sku Standard_LRS --nsg "TestApp-ngs" --vnet-name "TestApp-vnet" --subnet "TestApp-subnet" --private-ip-address 10.0.0.9
