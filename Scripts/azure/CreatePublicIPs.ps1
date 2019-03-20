9ba3226d9946dd8123d8559438e99fa680587359
Radu Vasile <radu.vasile@qiagen.com>
2019-03-20 11:43:49 +0200

    Update IPs;

--- Scripts/azure/CreatePublicIPs.ps1
+++ Scripts/azure/CreatePublicIPs.ps1
@@ -1,14 +1,17 @@
 # monolith 
-az network public-ip create --name "IP_Monolith" --resource-group TestApp --dns-name "testapp-monolith" --location "westeurope" --allocation-method Static --sku Basic --version IPv4 --tags "persistance=onetime"
+az network public-ip create --name "IP_Monolith" --resource-group TestApp --dns-name "testapp-monolith" --location "westeurope" --allocation-method Static --sku Standard --version IPv4 --zone 1 --tags "persistance=onetime"
 
 # service.analysis
-az network public-ip create --name "IP_Analysis" --resource-group TestApp --dns-name "testapp-service-analysis" --location "westeurope" --allocation-method Static --sku Basic --version IPv4 --tags "persistance=onetime"
+az network public-ip create --name "IP_Analysis" --resource-group TestApp --dns-name "testapp-service-analysis" --location "westeurope" --allocation-method Static --sku Standard --version IPv4 --zone 1 --tags "persistance=onetime"
 
 # service.auth
-az network public-ip create --name "IP_Auth" --resource-group TestApp --dns-name "testapp-service-auth" --location "westeurope" --allocation-method Static --sku Basic --version IPv4 --tags "persistance=onetime"
+az network public-ip create --name "IP_Auth" --resource-group TestApp --dns-name "testapp-service-auth" --location "westeurope" --allocation-method Static --sku Standard --version IPv4 --zone 1 --tags "persistance=onetime"
 
 # service.reports
-az network public-ip create --name "IP_Reports" --resource-group TestApp --dns-name "testapp-service-reports" --location "westeurope" --allocation-method Static --sku Basic --version IPv4 --tags "persistance=onetime"
+az network public-ip create --name "IP_Reports" --resource-group TestApp --dns-name "testapp-service-reports" --location "westeurope" --allocation-method Static --sku Standard --version IPv4 --zone 1 --tags "persistance=onetime"
 
 # service.files
-az network public-ip create --name "IP_Files" --resource-group TestApp --dns-name "testapp-service-files" --location "westeurope" --allocation-method Static --sku Basic --version IPv4 --tags "persistance=onetime"
+az network public-ip create --name "IP_Files" --resource-group TestApp --dns-name "testapp-service-files" --location "westeurope" --allocation-method Static --sku Standard --version IPv4 --zone 1 --tags "persistance=onetime"
+
+# jmeter
+az network public-ip create --name "IP_Jmeter" --resource-group TestApp --dns-name "testapp-jmeter" --location "westeurope" --allocation-method Static --sku Standard --version IPv4 --tags "persistance=onetime"
