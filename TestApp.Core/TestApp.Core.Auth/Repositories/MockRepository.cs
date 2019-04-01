8f0308f8eaf455a5ea99c640e4a306b729a5994b
Radu Vasile <radu.vasile@qiagen.com>
2019-04-01 22:26:37 +0300

    Added dummy repositories to mock dependencies (DB and storage)

---
+++ TestApp.Core/TestApp.Core.Auth/Repositories/MockRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using TestApp.Core.Auth.Interfaces;
+using TestApp.Core.Auth.Models;
+
+namespace TestApp.Core.Auth.Repositories
+{
+    public class MockRepository : IUserRepository, ITokenRepository
+    {
+        public Token Generate(string username)
+        {
+            return new Token
+            {
+                Id = Guid.NewGuid(),
+                Username = username,
+                CreatedAtUtc = DateTime.UtcNow
+            };
+        }
+
+        public Token GetToken(string tokenId)
+        {
+            return this.Generate(tokenId);
+        }
+
+        public void Remove(string tokenId)
+        {
+            // do nothing.
+        }
+
+        public Task<bool> ValidateCredentials(string username, string password)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
